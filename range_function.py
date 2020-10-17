48936dc5e755b8592daa14b7de5fe40dea01b0aa
rokpopov <rokpopov90@gmail.com>
2020-10-17 14:54:06 +0200

    Play with built-in range function

---
+++ range_function.py
@@ -0,0 +1,15 @@
+numbers = range(5)
+for number in numbers:
+    print(number)
+
+numbers2 = range(5, 10)
+for i in numbers2:
+    print(i)
+
+numbers3 = range(5, 10, 2)
+for n in numbers3:
+    print(n)
+
+
+for num in range(1, 5):
+    print(num)
